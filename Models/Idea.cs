01d5632323950d9286f28c643af101c5e98fc1fb
Jun Yu <shockz@me.com>
2020-12-27 18:17:07 +0900

    update

--- Models/Idea.cs
+++ Models/Idea.cs
@@ -4,6 +4,7 @@ namespace IdeaAppBlazor.Models
 {
     public class Idea
     {
+        [Required, Key]
         public int Id { get; set; }
         [Required]
         public string Note { get; set; }
